231d09b5ce1215236966cb0f4b8304dbb9c7498c
csmedley97 <csmedley@vt.edu>
2017-07-22 07:02:28 -0400

    Update SensorWeather.py

--- backend/SensorWeather.py
+++ backend/SensorWeather.py
@@ -15,6 +15,8 @@ class SensorWeather():
         device_last_temp = 0.0
         device_last_humidity = 0.0
         device_last_mag = 0.0
+        device_last_wind_speed = 0.0
+        device_last_rain_gauge = 0.0
         device_last_pressure = 0.0
         device_fixed = True
         device_update_interval = 50000
@@ -29,6 +31,8 @@ class SensorWeather():
                 self.device_last_humidity = device_sensor.get_humidity()
                 self.device_last_mag = get_compass()
                 self.device_last_pressure = get_pressure()
+                self.device_last_wind_speed = random.uniform(0.0, 20)
+                self.device_last_rain_guage = random.uniform(0.0, 10)
 
         def update_temp(self):
                 device_sensor = SenseHat()
@@ -51,13 +55,23 @@ class SensorWeather():
                 self.device_last_mag = device_sensor.get_compass()
                 self.device_last_time = time.time()
 
+        def update_rain(self):
+                self.device_last_rain_guage = 1.63
+                self.device_last_time = time.time()
+
+        def update_wind(self):
+                self.device_last_wind_speed = random.uniform(0.0, 20)
+                self.device_last_time = time.time()
+
         def update_all(self):
                 self.update_temp()
                 self.update_humidity()
+                self.update_wind()
+                self.update_rain()
                 self.update_pressure()
                 self.update_mag()
 
         def json_now(self):
                 self.update_all()
-                dict = {"ID": self.device_id, "Description": self.device_description, "Fixed": self.device_fixed, "Latitude": self.device_lat, "Longitude": self.device_lon, "Time": self.device_last_time, "Info": {"Temperature": self.device_last_temp, "Humidity": self.device_last_humidity, "Pressure": self.device_last_mag}}
+                dict = {"ID": self.device_id, "Description": self.device_description, "Fixed": self.device_fixed, "Latitude": self.device_lat, "Longitude": self.device_lon, "Time": self.device_last_time, "Info": {"Temperature": self.device_last_temp, "Humidity": self.device_last_humidity, "Pressure": self.device_last_pressure, "Magnetometer": self.device_last_mag, "RainGauge": self.device_last_rain_guage, "WindSpeed": self.device_last_wind_speed}}
                 return dict
