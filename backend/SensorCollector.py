19bfc25ff900647a5572ec8ca75934704c5f5d44
ofr8601 <ofr8601@gmail.com>
2017-07-22 08:21:36 +0000

    Add files via upload

--- backend/SensorCollector.py
+++ backend/SensorCollector.py
@@ -1,18 +1,28 @@
-from sense_hat import SenseHat
+import time
+from sense_hat import SenseHat, ACTION_PRESSED, ACTION_HELD,ACTION_RELEASED
 import sys
-sensor = SenseHat()
-
 sys.path.insert(0, "/home/pi/workspace/iot/Common")
 from SensorMsg import SensorMsg
-sensorMsg = SensorMsg()
-sensorMsg.setDeviceID(101)
-sensorMsg.setDeviceLatitude (12.34567)
-sensorMsg.setDeviceLongitude(-123.4567)
 
-while True:
+sensor    = SenseHat()
+sensorMsg = SensorMsg()
+sensorMsg.setDeviceID(102)
+sensorMsg.setDeviceLatitude (42.504905)
+sensorMsg.setDeviceLongitude(-71.236539)
+
+while True:	
+	
 	temp = sensor.get_temperature()
 	pressure = sensor.get_pressure()
 	humidity = sensor.get_humidity()
+        orientation = sensor.get_orientation()
+
+	#round to nearest 1/10th
+        temp = round (temp, 1)
+        pressure =round( pressure, 1)
+        humidity = round (humidity, 1)
+	#convert temp to fahrenheit
+	temp = temp * 1.8 +32
 
 	sensorMsg.setSensorType(1)
 	sensorMsg.setSensorValue(temp)
@@ -20,17 +30,24 @@ while True:
 	
 	msg = sensorMsg.toString()
 
-	sensor.show_message(msg, scroll_speed = 0.10)
-	time.sleep(5)
-
-	
-	#round to nearest 1/10th
-	temp = round (temp, 1)
-	pressure =round( pressure, 1)
-	humidity = round (humidity, 1)
-	# create message
-	msg = "Temp=%s, Pressure= %s, Humidity = %s" %(temp, pressure, humidity) 
-	#speed in secs
 	sensor.show_message(msg, scroll_speed = 0.05)
-
 	time.sleep(5)
+
+	sensorMsg.setSensorType(2)
+        sensorMsg.setSensorValue(pressure)
+        sensorMsg.updateTime()
+
+        msg = sensorMsg.toString()
+
+        sensor.show_message(msg, scroll_speed = 0.05)
+        time.sleep(5)
+
+	sensorMsg.setSensorType(3)
+        sensorMsg.setSensorValue(humidity)
+        sensorMsg.updateTime()
+
+        msg = sensorMsg.toString()
+
+        sensor.show_message(msg, scroll_speed = 0.05)
+        time.sleep(5)
+
